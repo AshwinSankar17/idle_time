6cc1f4d70f47c98c7b6ba8dc72e7c6266cf9c6cd
S Ashwin <47474911+TheSinOfSloth@users.noreply.github.com>
2020-01-17 21:32:05 +0530

    Add files via upload

---
+++ idleTime/idle.py
@@ -0,0 +1,25 @@
+import platform
+import os
+
+if (platform.system() == 'Windows'):
+    from ctypes import Structure, windll, c_uint, sizeof, byref
+    
+    class LASTINPUTINFO(Structure):
+        _fields_ = [
+            ('cbSize', c_uint),
+            ('dwTime', c_uint),
+        ]
+ 
+    def get_idle_duration():
+        lastInputInfo = LASTINPUTINFO()
+        lastInputInfo.cbSize = sizeof(lastInputInfo)
+        windll.user32.GetLastInputInfo(byref(lastInputInfo))
+        millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+        return millis / 1000.0
+
+
+else:
+    def get_idle_duration():
+        millis = os.system('sudo -u whoami xprintidle')
+        os.system('clear')
+        return millis / 1000.0
